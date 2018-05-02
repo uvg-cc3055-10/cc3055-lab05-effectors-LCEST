68d0248b4833f26b97de45c9888a56e896fac217
LCEST <lcesturban10@gmail.com.com>
2018-05-02 09:28:31 -0600

    Final

---
+++ Assets/Scripts/AreaEffector.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEffector : MonoBehaviour {
+
+    private float time=0;
+    private bool prueba = false;
+    public GameObject areaEffector;
+	// Use this for initialization
+	void Start () {
+        prueba = false;
+        time = 0;
+	}
+
+    // Update is called once per frame
+    void Update() {
+        time += Time.deltaTime;
+
+        if(time>3){
+            areaEffector.SetActive(prueba);
+            time = 0;
+            prueba = !prueba;
+        }
+    }
+	
+}
